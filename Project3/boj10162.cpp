00b89b92cd03bd7d920663b855b5e67dbd295861
rkdwlss2 <rkdwlss2@gmail.com>
2021-06-09 22:30:47 +0900

    boj2688

--- Project3/boj10162.cpp
+++ Project3/boj10162.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int coin[3] = { 300,60,10 };
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	vector<int> v;
-	for (int i = 0; i < 3; i++) {
-		int answer=n / coin[i];
-		v.push_back(answer);
-		n -= answer * coin[i];
-	}
-	if (n > 0)cout << -1 << '\n';
-	else {
-		for (auto i : v) {
-			cout << i << ' ';
-		}
-	}
-
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int coin[3] = { 300,60,10 };
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	vector<int> v;
+//	for (int i = 0; i < 3; i++) {
+//		int answer=n / coin[i];
+//		v.push_back(answer);
+//		n -= answer * coin[i];
+//	}
+//	if (n > 0)cout << -1 << '\n';
+//	else {
+//		for (auto i : v) {
+//			cout << i << ' ';
+//		}
+//	}
+//
+//}
---
+++ Project3/boj2688.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define lli long long int
+using namespace std;
+
+lli cache[65][11];
+lli n;
+lli dp(lli index, lli prev) {
+	if (index == n)return 1;
+	lli& ret = cache[index][prev];
+	if (ret != -1)return ret;
+	ret = 0;
+	for (lli next=prev;next<10;next++){
+		ret += dp(index + 1, next);
+	}
+	return ret;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	lli tc;
+	cin >> tc;
+	while (tc--) {
+		for (lli i = 0; i < 65; i++) {
+			fill(cache[i], cache[i] + 11, -1);
+		}
+		cin >> n;
+		cout << dp(0, 0) << '\n';
+	}
+}
