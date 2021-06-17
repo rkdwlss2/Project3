3e49c35eab424086b5bf684ce727340eccb283cb
rkdwlss2 <rkdwlss2@gmail.com>
2021-06-18 00:17:54 +0900

    boj1212

--- Project3/boj1032.cpp
+++ Project3/boj1032.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int arr[51][28];
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	int length = -1;
-	for (int i = 0; i < n; i++) {
-		string tmp;
-		cin >> tmp;
-		length=tmp.size();
-		for (int j = 0; j < tmp.size(); j++) {
-			if (tmp[j] == '.') {
-				arr[j][27]++;
-				continue;
-			}
-			arr[j][tmp[j] - 'a']++;
-		}
-	}
-	for (int i = 0; i < length; i++) {
-		bool ok = false;
-		for (int j = 0; j < 26; j++) {
-			if (arr[i][j] == n) {
-				char Out = j + 'a';
-				cout << Out;
-				ok = true;
-			}
-		}
-		if (arr[i][27] == n) {
-			cout << '.';
-			ok = true;
-		}
-		if (!ok)cout << '?';
-	}
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int arr[51][28];
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	int length = -1;
+//	for (int i = 0; i < n; i++) {
+//		string tmp;
+//		cin >> tmp;
+//		length=tmp.size();
+//		for (int j = 0; j < tmp.size(); j++) {
+//			if (tmp[j] == '.') {
+//				arr[j][27]++;
+//				continue;
+//			}
+//			arr[j][tmp[j] - 'a']++;
+//		}
+//	}
+//	for (int i = 0; i < length; i++) {
+//		bool ok = false;
+//		for (int j = 0; j < 26; j++) {
+//			if (arr[i][j] == n) {
+//				char Out = j + 'a';
+//				cout << Out;
+//				ok = true;
+//			}
+//		}
+//		if (arr[i][27] == n) {
+//			cout << '.';
+//			ok = true;
+//		}
+//		if (!ok)cout << '?';
+//	}
+//}
