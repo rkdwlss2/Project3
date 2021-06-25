973b8ab48d7f537432b3639d0b38bc481dd0b5fb
rkdwlss2 <rkdwlss2@gmail.com>
2021-06-25 23:26:49 +0900

    boj2711

--- Project3/boj3986.cpp
+++ Project3/boj3986.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	string str1, str2;
-	cin >> str1 >> str2;
-	int slen = str1.size();
-	int slen2 = str2.size();
-	if (slen >= slen2)cout << "go" << '\n';
-	else cout << "no" << '\n';
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	string str1, str2;
+//	cin >> str1 >> str2;
+//	int slen = str1.size();
+//	int slen2 = str2.size();
+//	if (slen >= slen2)cout << "go" << '\n';
+//	else cout << "no" << '\n';
+//}
