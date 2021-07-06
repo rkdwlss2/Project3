7787e07917eae851d1e8414ad1ee4136527688ab
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-06 22:37:59 +0900

    boj19532

--- Project3/boj1264.cpp
+++ Project3/boj1264.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-#include <string>
-using namespace std;
-char arr[10] = { 'a','e','i','o','u','A','E','I','O','U'};
-int main() {
-	while (true) {
-		string str;
-		getline(cin, str);
-		if (str == "#")break;
-		int cnt = 0;
-		for (int i = 0; i < str.size(); i++) {
-			for (int j = 0; j < 10; j++) {
-				if (str[i] == arr[j])cnt++;
-			}
-		}
-		cout << cnt << '\n';
-	}
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//char arr[10] = { 'a','e','i','o','u','A','E','I','O','U'};
+//int main() {
+//	while (true) {
+//		string str;
+//		getline(cin, str);
+//		if (str == "#")break;
+//		int cnt = 0;
+//		for (int i = 0; i < str.size(); i++) {
+//			for (int j = 0; j < 10; j++) {
+//				if (str[i] == arr[j])cnt++;
+//			}
+//		}
+//		cout << cnt << '\n';
+//	}
+//}
---
+++ Project3/boj19532.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+int a, b, c, d, e, f;
+int x, y;
+void go(int index) {
+	if (index == 2) {
+		int ansFirst = a * v[0] + b * v[1];
+		int ansSecond = d * v[0] + e * v[1];
+		if (ansFirst == c && ansSecond == f) {
+			x = v[0];
+			y = v[1];
+		}
+		return;
+	}
+	for (int i = -999; i < 1000; i++){
+		v.push_back(i);
+		go(index + 1);
+		v.pop_back();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> a >> b >> c >> d >> e >> f;
+	go(0);
+	cout << x << ' ' << y << '\n';
+}
