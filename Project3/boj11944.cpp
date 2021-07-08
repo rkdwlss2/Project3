eb3bfe694b3e0224ec2004bb26992bdac31d7875
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-08 22:30:42 +0900

    boj6231

--- Project3/boj11944.cpp
+++ Project3/boj11944.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	string a, b;
-	int n, m;
-	cin >> a >> b;
-	n = atoi(a.c_str());
-	m = atoi(b.c_str());
-	int c=0;
-	int aSize = a.size();
-	if (aSize * n <= m) {
-		for (int i = 0; i < n*aSize; i++) {
-			cout << a[c];
-			c++;
-			c = (c + aSize) % aSize;
-		}
-	}
-	else {
-		for (int i = 0; i < m; i++) {
-				cout << a[c];
-			c++;
-			c = (c + aSize) % aSize;
-		}
-	}
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	string a, b;
+//	int n, m;
+//	cin >> a >> b;
+//	n = atoi(a.c_str());
+//	m = atoi(b.c_str());
+//	int c=0;
+//	int aSize = a.size();
+//	if (aSize * n <= m) {
+//		for (int i = 0; i < n*aSize; i++) {
+//			cout << a[c];
+//			c++;
+//			c = (c + aSize) % aSize;
+//		}
+//	}
+//	else {
+//		for (int i = 0; i < m; i++) {
+//				cout << a[c];
+//			c++;
+//			c = (c + aSize) % aSize;
+//		}
+//	}
+//}
---
+++ Project3/boj6231.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "String #" << i + 1 << '\n';
+		string str;
+		cin >> str;
+		for (int j = 0; j < str.size(); j++) {
+			char tmp = str[j];
+			if (tmp == 'Z') {
+				cout << 'A';
+			}
+			else {
+				tmp++;
+				cout << tmp;
+			}
+		}
+		cout << '\n';
+		cout << '\n';
+	}
+}
