8ad97c43403184c3cbbb9fc45b628df144536fa6
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-11 00:34:09 +0900

    boj20058

--- Project3/boj2576.cpp
+++ Project3/boj2576.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int sum = 0;
-	int min = 101;
-	for (int i = 0; i < 7; i++) {
-		int tmp;
-		cin >> tmp;
-		if (tmp % 2 == 1) {
-			sum += tmp;
-			if (tmp < min)min = tmp;
-		}
-	}
-	if (sum == 0) {
-		cout << -1 << '\n';
-	}
-	else {
-		cout << sum << '\n';
-		cout << min << '\n';
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int sum = 0;
+//	int min = 101;
+//	for (int i = 0; i < 7; i++) {
+//		int tmp;
+//		cin >> tmp;
+//		if (tmp % 2 == 1) {
+//			sum += tmp;
+//			if (tmp < min)min = tmp;
+//		}
+//	}
+//	if (sum == 0) {
+//		cout << -1 << '\n';
+//	}
+//	else {
+//		cout << sum << '\n';
+//		cout << min << '\n';
+//	}
+//}
