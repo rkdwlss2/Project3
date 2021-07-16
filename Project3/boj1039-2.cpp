5ccb92a14ec42fc3fe75e13903b35d181901bc4a
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-16 22:50:52 +0900

    boj1039

---
+++ Project3/boj1039-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+using namespace std;
+int m, k;
+string n;
+int ck[1000001];
+int cnt = 0;
+int d[1000001][11];
+
+int dfs(string a, int depth)
+{
+    if (depth == k) {
+        return stoi(a);
+    }
+
+    int& ret = d[stoi(a)][depth];
+    if (ret != -1)return ret;
+
+    for (int i = 0; i < a.size() - 1; i++)
+    {
+        for (int j = i + 1; j < a.size(); j++)
+        {
+
+            swap(a[i], a[j]);
+            if (a[0] == '0') {
+                swap(a[i], a[j]);
+                continue;
+            }
+            ret = max(ret, dfs(a, depth + 1));
+            swap(a[i], a[j]);
+        }
+    }
+    return ret;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> k;
+    memset(d, -1, sizeof(d));
+    cout << dfs(n, 0) << '\n';
+}
--- Project3/boj2004.cpp
+++ Project3/boj2004.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int count25(int num,int mod) {
-	int val = num;
-	int cnt = 0;
-	while (val > 0) {
-		if (mod == 2) {
-			val /= 2;
-		}
-		if (mod == 5) {
-			val /= 5;
-		}
-		cnt += val;
-	}
-	return cnt;
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	int n2 = count25(n, 2) - count25(m, 2) - count25(n - m, 2);
-	int n5 = count25(n, 5) - count25(m, 5) - count25(n - m, 5);
-	cout << min(n2, n5) << '\n';
-}
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int count25(int num,int mod) {
+//	int val = num;
+//	int cnt = 0;
+//	while (val > 0) {
+//		if (mod == 2) {
+//			val /= 2;
+//		}
+//		if (mod == 5) {
+//			val /= 5;
+//		}
+//		cnt += val;
+//	}
+//	return cnt;
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n, m;
+//	cin >> n >> m;
+//	int n2 = count25(n, 2) - count25(m, 2) - count25(n - m, 2);
+//	int n5 = count25(n, 5) - count25(m, 5) - count25(n - m, 5);
+//	cout << min(n2, n5) << '\n';
+//}
