2ea962e0ce94dbc83c48d613a82032855ed01438
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-24 14:11:55 +0900

    boj1009

---
+++ Project3/boj1009.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	while(n--) {
+		int a, b;
+		cin >> a >> b;
+		int answer = 1;
+		while (b--) {
+			answer *= a;
+			answer %= 10;
+		}
+		if (answer == 0) {
+			cout << 10 << '\n';
+		}
+		else {
+			cout << answer << '\n';
+		}
+		
+	}
+}
--- Project3/boj2470.cpp
+++ Project3/boj2470.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-using namespace std;
-
-vector<int> v;
-int anx, any;
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
-		v.push_back(tmp);
-	}
-	sort(v.begin(), v.end());
-	int left = 0;
-	int right = n - 1;
-	anx = v[left];
-	any = v[right];
-	int answer = (anx + any > 0) ? (anx + any) : (any + anx);
-	while (left < right) {
-		
-		int ax = v[left];
-		int ay = v[right];
-		int now= (ax + ay);
-		if (abs(now) < abs(answer)) {
-			answer = now;
-			anx = ax;
-			any = ay;
-		}
-		if (now > 0) {
-			right--;
-		}
-		else {
-			left++;
-		}
-
-	}
-	cout << anx <<" "<< any << '\n';
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <cmath>
+//using namespace std;
+//
+//vector<int> v;
+//int anx, any;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		int tmp;
+//		cin >> tmp;
+//		v.push_back(tmp);
+//	}
+//	sort(v.begin(), v.end());
+//	int left = 0;
+//	int right = n - 1;
+//	anx = v[left];
+//	any = v[right];
+//	int answer = (anx + any > 0) ? (anx + any) : (any + anx);
+//	while (left < right) {
+//		
+//		int ax = v[left];
+//		int ay = v[right];
+//		int now= (ax + ay);
+//		if (abs(now) < abs(answer)) {
+//			answer = now;
+//			anx = ax;
+//			any = ay;
+//		}
+//		if (now > 0) {
+//			right--;
+//		}
+//		else {
+//			left++;
+//		}
+//
+//	}
+//	cout << anx <<" "<< any << '\n';
+//}
