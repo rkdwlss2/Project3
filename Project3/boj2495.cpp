68328002d797ffd5231f5a69aadfc36dc7be6c45
rkdwlss2 <rkdwlss2@gmail.com>
2021-07-28 10:34:04 +0900

    boj5427

--- Project3/boj2495.cpp
+++ Project3/boj2495.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	for (int i = 0; i < 3; i++) {
-		string str;
-		cin >> str;
-		int big = -1;
-		for (int j = 0; j < 8; j++) {
-			int num = 1;
-			char now = str[j];
-			for (int k = j+1; k < 8; k++) {
-				char next = str[k];
-				if (now == next)num++;
-				else break;
-			}
-			if (num > big)big = num;
-		}
-		cout << big << '\n';
-	}
-	
-
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	for (int i = 0; i < 3; i++) {
+//		string str;
+//		cin >> str;
+//		int big = -1;
+//		for (int j = 0; j < 8; j++) {
+//			int num = 1;
+//			char now = str[j];
+//			for (int k = j+1; k < 8; k++) {
+//				char next = str[k];
+//				if (now == next)num++;
+//				else break;
+//			}
+//			if (num > big)big = num;
+//		}
+//		cout << big << '\n';
+//	}
+//	
+//
+//}
