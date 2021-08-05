b9c48366132fd5c7d51977a622b7a975dc0a9b80
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-05 20:24:09 +0900

    boj3613

--- Project3/boj1342.cpp
+++ Project3/boj1342.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#define lli long long int
-using namespace std;
-string aa;
-lli n;
-int visited[27];
-lli answer = 0;
-unordered_map<lli, lli> um;
-void go(lli index,lli prev) {
-	if (index == n) {
-		answer++;
-		return;
-	}
-	for (lli i = 0; i < 26; i++) {
-		if (visited[i] == 0)continue;
-		visited[i]--;
-		if (index != 0 && prev == i) {
-			visited[i]++;
-			continue;
-		}
-		go(index + 1,i);
-		visited[i]++;
-
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> aa;
-	n = aa.size();
-	for (int i = 0; i < aa.size(); i++) {
-		int tmp = aa[i] - 'a';
-		visited[tmp]++;
-	}
-	go(0,-1);
-	cout << answer << '\n';
-}
+//#include <iostream>
+//#include <string>
+//#include <unordered_map>
+//#define lli long long int
+//using namespace std;
+//string aa;
+//lli n;
+//int visited[27];
+//lli answer = 0;
+//unordered_map<lli, lli> um;
+//void go(lli index,lli prev) {
+//	if (index == n) {
+//		answer++;
+//		return;
+//	}
+//	for (lli i = 0; i < 26; i++) {
+//		if (visited[i] == 0)continue;
+//		visited[i]--;
+//		if (index != 0 && prev == i) {
+//			visited[i]++;
+//			continue;
+//		}
+//		go(index + 1,i);
+//		visited[i]++;
+//
+//	}
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cin >> aa;
+//	n = aa.size();
+//	for (int i = 0; i < aa.size(); i++) {
+//		int tmp = aa[i] - 'a';
+//		visited[tmp]++;
+//	}
+//	go(0,-1);
+//	cout << answer << '\n';
+//}
---
+++ Project3/boj3613.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string str;
+	cin >> str;
+	string ans = "";
+	bool Ok = false;
+	bool Java=false;
+	bool Clan = false;
+	if (str[0] == '_' || str[str.size() - 1] == '_' || (str[0] >= 'A' && str[0] <= 'Z')) {
+		cout << "Error!" << '\n';
+		exit(0);
+	}
+	for (int i = 0; i < str.size(); i++) {
+		char now = str[i];
+		int nowInt = 0;
+		if (Ok) {
+			if (now == '_') {
+				cout << "Error!" << '\n';
+				exit(0);
+			}
+			if (now >= 'A' && now <= 'Z') {
+				cout << "Error!" << '\n';
+				exit(0);
+			}
+			ans += (now-'a' + 'A');
+			Ok = false;
+			continue;
+		}
+		if (!Ok&&now >= 'A' && now <= 'Z') {
+			Java = true;
+			nowInt = str[i] - 'A';
+			ans += ('_');
+			ans += (nowInt + 'a');
+		}
+		else if (!Ok&&now >= 'a' && now <= 'z') {
+			nowInt = str[i] - 'a';
+			ans += (nowInt + 'a');
+		}
+		else if (now=='_'){
+			Clan = true;
+			Ok = true;
+			continue;
+		}
+	}
+	if (Java&&Clan) {
+		cout << "Error!" << '\n';
+	}else{
+		cout << ans << '\n';
+	}
+	
+}
