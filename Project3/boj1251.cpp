24c84b4db999767fbfca3c21f072bdae8f3330e6
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-06 21:57:53 +0900

    boj1251

---
+++ Project3/boj1251.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+string Swap(string str) {
+	int n = str.size();
+	string tmp = "";
+	for (int i = n - 1; i >= 0; i--) {
+		tmp += str[i];
+	}
+	return tmp;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string str;
+	cin >> str;
+	int n = str.size();
+	priority_queue<string,vector<string>,greater<string>> pq;
+	for (int i = 1; i < n-1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			string a = "", b = "", c = "";
+			for (int k = 0; k < i; k++) {
+				a += str[k];
+			}
+			for (int k = i; k < j; k++) {
+				b += str[k];
+			}
+			for (int k = j; k < n; k++) {
+				c += str[k];
+			}
+			a = Swap(a);
+			b = Swap(b);
+			c = Swap(c);
+			string now = a + b + c;
+			pq.push(now);
+		}
+	}
+	cout << pq.top() << '\n';
+}
--- Project3/boj2607.cpp
+++ Project3/boj2607.cpp
@@ -1,69 +1,69 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int firstStr[26];
-int tmpStr[26];
-int extStr[26];
-void setString(string inStr) {
-	for (int k = 0; k < 26; k++) {
-		tmpStr[k] = 0;
-	}
-	for (int i = 0; i < inStr.size(); i++) {
-		int tmp= inStr[i] - 'A';
-		tmpStr[tmp]++;
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	string str;
-	cin >> str;
-	int strSize = str.size();
-	for (int i = 0; i < strSize; i++) {
-		int tmp = str[i] - 'A';
-		firstStr[tmp]++;
-	}
-	int ans = 0;
-	
-	for (int i = 0; i < n - 1; i++) {
-		string now;
-		cin >> now;
-		int nowSize = now.size();
-		setString(now);
-		int cnt = 0;
-		for (int j = 0; j < 26; j++) {
-			if (firstStr[j] > 0 && tmpStr[j] > 0)cnt += ((firstStr[j] > tmpStr[j]) ? tmpStr[j] : firstStr[j]);
-		}
-		int bigSize = (nowSize > strSize) ? nowSize : strSize;
-		cnt = bigSize - cnt;
-
-		if (nowSize - 1 == strSize) {
-			if (cnt == 1) {
-				ans++;
-			}
-		}
-		if (nowSize+1== strSize) {
-			if (cnt == 1) {
-				ans++;
-			}
-		}
-		if (nowSize == strSize) {
-			if (cnt == 0)ans++;
-		}
-		//if (cnt==0&&(strSize==nowSize)) {
-		//	int cnt2 = 0;
-		//	for (int k = 0; k < strSize; k++) {
-		//		if (now[k] != str[k])cnt2++;
-		//	}
-		//	if (cnt2 == 2)ans++;
-		//	continue;
-		//}
-		if (strSize == nowSize && cnt == 1) {
-			ans++;
-		}
-	}
-	cout << ans << '\n';
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int firstStr[26];
+//int tmpStr[26];
+//int extStr[26];
+//void setString(string inStr) {
+//	for (int k = 0; k < 26; k++) {
+//		tmpStr[k] = 0;
+//	}
+//	for (int i = 0; i < inStr.size(); i++) {
+//		int tmp= inStr[i] - 'A';
+//		tmpStr[tmp]++;
+//	}
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	string str;
+//	cin >> str;
+//	int strSize = str.size();
+//	for (int i = 0; i < strSize; i++) {
+//		int tmp = str[i] - 'A';
+//		firstStr[tmp]++;
+//	}
+//	int ans = 0;
+//	
+//	for (int i = 0; i < n - 1; i++) {
+//		string now;
+//		cin >> now;
+//		int nowSize = now.size();
+//		setString(now);
+//		int cnt = 0;
+//		for (int j = 0; j < 26; j++) {
+//			if (firstStr[j] > 0 && tmpStr[j] > 0)cnt += ((firstStr[j] > tmpStr[j]) ? tmpStr[j] : firstStr[j]);
+//		}
+//		int bigSize = (nowSize > strSize) ? nowSize : strSize;
+//		cnt = bigSize - cnt;
+//
+//		if (nowSize - 1 == strSize) {
+//			if (cnt == 1) {
+//				ans++;
+//			}
+//		}
+//		if (nowSize+1== strSize) {
+//			if (cnt == 1) {
+//				ans++;
+//			}
+//		}
+//		if (nowSize == strSize) {
+//			if (cnt == 0)ans++;
+//		}
+//		//if (cnt==0&&(strSize==nowSize)) {
+//		//	int cnt2 = 0;
+//		//	for (int k = 0; k < strSize; k++) {
+//		//		if (now[k] != str[k])cnt2++;
+//		//	}
+//		//	if (cnt2 == 2)ans++;
+//		//	continue;
+//		//}
+//		if (strSize == nowSize && cnt == 1) {
+//			ans++;
+//		}
+//	}
+//	cout << ans << '\n';
+//}
