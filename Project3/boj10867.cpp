7fc723ac44ab0255c449d282d90857eb686675d1
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-12 22:38:01 +0900

    boj17413

--- Project3/boj10867.cpp
+++ Project3/boj10867.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <set>
-using namespace std;
-
-int main() {
-	int n;
-	cin >> n;
-	set<int> Set;
-	for (int i = 0; i < n; i++) {
-		int now;
-		cin >> now;
-		Set.insert(now);
-	}
-	for (auto i : Set) {
-		cout << i <<' ';
-	}
-}
+//#include <iostream>
+//#include <set>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//	set<int> Set;
+//	for (int i = 0; i < n; i++) {
+//		int now;
+//		cin >> now;
+//		Set.insert(now);
+//	}
+//	for (auto i : Set) {
+//		cout << i <<' ';
+//	}
+//}
---
+++ Project3/boj4458.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n + 1; i++) {	
+//		string str;
+//		getline(cin, str);
+//		if (i == 0)continue;
+//		if (str[0] >= 'a' && str[0] <= 'z')str[0] = str[0] - 'a' + 'A';
+//		cout << str << '\n';
+//	}
+//}
