62b5ef4ca6974d2a71e6b86cc87734cd6ddb4bca
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-19 23:02:52 +0900

    boj13913

---
+++ Project3/boj13913-2.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int dx[3] = { -1, 1, 2 };
+int board[200001];
+int parent[200001];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+    queue<int> q;
+    q.push(N);
+    board[N] = 1;
+    parent[N] = N;
+    if (N == K) {
+        cout << 0 << '\n';
+        cout << N << '\n';
+        return 0;
+    }
+    while (!q.empty())
+    {
+        int cur = q.front(); q.pop();
+        for (int i = 0; i < 3; i++)
+        {
+            int nx;
+            if (i != 2) nx = cur + dx[i];
+            else nx = cur * dx[i];
+            if (nx < 0 || nx >= 200000 || board[nx]) continue;
+            q.push(nx);
+            board[nx] = board[cur] + 1;
+            parent[nx] = cur;
+            if (nx == K) {
+                cout << board[nx] - 1 << '\n';
+                int value = K;
+                vector<int> answer;
+                while (true)
+                {
+                    if (value != N) {
+                        answer.push_back(value);
+                        value = parent[value];
+                    }
+                    else {
+                        answer.push_back(N);
+                        break;
+                    }
+                }
+                reverse(answer.begin(), answer.end());
+                for (auto a : answer) cout << a << ' ';
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
--- Project3/proGrid.cpp
+++ Project3/proGrid.cpp
@@ -1,87 +1,87 @@
-#include <iostream>
-#include <list>
-#include <vector>
-#include <string>
-#include <stack>
-using namespace std;
-int arr[1000001];
-list<int> li;
-list<int>::iterator iter;
-list<int>::iterator its[1000001];
-stack<pair<int, int>> st;
-int stringToNum(string str) {
-	string tmp = "";
-	for (int i = 2; i < str.size(); i++) {
-		tmp += str[i];
-	}
-	return atoi(tmp.c_str());
-}
-
-string solution(int n, int k, vector<string> cmd) {
-	string answer = "";
-
-	for (int i = 0; i < n; i++) {
-		li.push_back(i);
-	}
-	li.push_back(n);
-	iter = li.begin();
-	int tmp = k;
-	for (int i = 0 ; i <n+1;i++){
-		its[i] = iter;
-		iter++;
-	}
-	iter = its[k];
-	for (int i = 0; i < (int)cmd.size(); i++) {
-		if (cmd[i][0] == 'D') {
-			int num = stringToNum(cmd[i]);
-			while (num--) {
-				iter++;
-			}
-			
-		}else if (cmd[i][0] == 'U') {
-			int num = stringToNum(cmd[i]);
-			while (num--) {
-				iter--;
-			}
-		}
-		else if (cmd[i][0] == 'C') {
-			//
-			//if (++iter == li.end()) {
-			//	st.push({ iter,*tmpiter });
-			//	iter--;
-			//	iter--;
-			//}
-			//else {
-			//	st.push({ iter,*tmpiter });
-			//}
-			//li.erase(tmpiter);
-			st.push({ *iter,*(next(iter)) });
-			iter = li.erase(iter);
-			if (*iter ==n) {
-				iter--;
-			}
-		}
-		else if(cmd[i][0]=='Z') {
-			int last = st.top().first;
-			int index = st.top().second;
-			st.pop();
-			its[last]=li.insert(its[index], last);
-		}
-
-	}
-	string status(n, 'X');
-	for (auto i : li) {
-		if(i!=n)status[i] = 'O';
-	}
-
-	return status;
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int a=8, b=7;
-	vector<string> cmd = { "C","C","C","Z","Z" };
-	cout<<solution(a,b,cmd)<<'\n';
-
-}
+//#include <iostream>
+//#include <list>
+//#include <vector>
+//#include <string>
+//#include <stack>
+//using namespace std;
+//int arr[1000001];
+//list<int> li;
+//list<int>::iterator iter;
+//list<int>::iterator its[1000001];
+//stack<pair<int, int>> st;
+//int stringToNum(string str) {
+//	string tmp = "";
+//	for (int i = 2; i < str.size(); i++) {
+//		tmp += str[i];
+//	}
+//	return atoi(tmp.c_str());
+//}
+//
+//string solution(int n, int k, vector<string> cmd) {
+//	string answer = "";
+//
+//	for (int i = 0; i < n; i++) {
+//		li.push_back(i);
+//	}
+//	li.push_back(n);
+//	iter = li.begin();
+//	int tmp = k;
+//	for (int i = 0 ; i <n+1;i++){
+//		its[i] = iter;
+//		iter++;
+//	}
+//	iter = its[k];
+//	for (int i = 0; i < (int)cmd.size(); i++) {
+//		if (cmd[i][0] == 'D') {
+//			int num = stringToNum(cmd[i]);
+//			while (num--) {
+//				iter++;
+//			}
+//			
+//		}else if (cmd[i][0] == 'U') {
+//			int num = stringToNum(cmd[i]);
+//			while (num--) {
+//				iter--;
+//			}
+//		}
+//		else if (cmd[i][0] == 'C') {
+//			//
+//			//if (++iter == li.end()) {
+//			//	st.push({ iter,*tmpiter });
+//			//	iter--;
+//			//	iter--;
+//			//}
+//			//else {
+//			//	st.push({ iter,*tmpiter });
+//			//}
+//			//li.erase(tmpiter);
+//			st.push({ *iter,*(next(iter)) });
+//			iter = li.erase(iter);
+//			if (*iter ==n) {
+//				iter--;
+//			}
+//		}
+//		else if(cmd[i][0]=='Z') {
+//			int last = st.top().first;
+//			int index = st.top().second;
+//			st.pop();
+//			its[last]=li.insert(its[index], last);
+//		}
+//
+//	}
+//	string status(n, 'X');
+//	for (auto i : li) {
+//		if(i!=n)status[i] = 'O';
+//	}
+//
+//	return status;
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int a=8, b=7;
+//	vector<string> cmd = { "C","C","C","Z","Z" };
+//	cout<<solution(a,b,cmd)<<'\n';
+//
+//}
