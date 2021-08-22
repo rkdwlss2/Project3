ccd6198404587759f5eb58effda5b1b642182b80
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-22 22:09:48 +0900

    boj15829

---
+++ Project3/boj15829.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#define lli long long int
+using namespace std;
+lli mod = 1234567891;
+lli r = 31;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	lli n;
+	cin >> n;
+	string str;
+	cin >> str;
+	lli answer = 0;
+	lli Mul = 1;
+	for (lli i = 0; i < str.size(); i++) {
+		char tmp = str[i];
+		lli tmpInt = str[i] - 'a'+1
+			;
+		answer = (answer%mod+((tmpInt % mod) * Mul)%mod) % mod;
+		Mul=(Mul* r)%mod;
+	}
+	cout << answer << '\n';
+}
