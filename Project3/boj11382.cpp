3951e631488622c422ef0c2f0caaf74983dc41f5
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-24 22:43:38 +0900

    boj2851

--- Project3/boj11382.cpp
+++ Project3/boj11382.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#define lli long long int
-using namespace std;
-
-int main() {
-	lli a, b, c;
-	cin >> a >> b >> c;
-	cout << a + b + c << '\n';
-}
+//#include <iostream>
+//#define lli long long int
+//using namespace std;
+//
+//int main() {
+//	lli a, b, c;
+//	cin >> a >> b >> c;
+//	cout << a + b + c << '\n';
+//}
