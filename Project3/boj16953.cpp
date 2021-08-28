b7f565adb2a2559e7f631f634c38d5273dc72c81
rkdwlss2 <rkdwlss2@gmail.com>
2021-08-28 21:23:24 +0900

    boj3028

--- Project3/boj16953.cpp
+++ Project3/boj16953.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <queue>
-#define lli long long int
-using namespace std;
-lli m, k;
-
-void bfs() {
-	queue<pair<lli, lli>> q;
-	q.push({ m,0 });
-	while (!q.empty()) {
-		auto now = q.front(); q.pop();
-		if (now.first == k) {
-			cout << now.second + 1 << '\n';
-			exit(0);
-		}
-		if (now.first * 2 <= k) {
-			q.push({ now.first * 2,now.second + 1 });
-		}
-		if (now.first * 10+1 <= k) {
-			q.push({ now.first * 10+1,now.second + 1 });
-		}
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> m >> k;
-	bfs();
-	cout << -1;
-}
+//#include <iostream>
+//#include <queue>
+//#define lli long long int
+//using namespace std;
+//lli m, k;
+//
+//void bfs() {
+//	queue<pair<lli, lli>> q;
+//	q.push({ m,0 });
+//	while (!q.empty()) {
+//		auto now = q.front(); q.pop();
+//		if (now.first == k) {
+//			cout << now.second + 1 << '\n';
+//			exit(0);
+//		}
+//		if (now.first * 2 <= k) {
+//			q.push({ now.first * 2,now.second + 1 });
+//		}
+//		if (now.first * 10+1 <= k) {
+//			q.push({ now.first * 10+1,now.second + 1 });
+//		}
+//	}
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cin >> m >> k;
+//	bfs();
+//	cout << -1;
+//}
---
+++ Project3/boj3028.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int now = 0;
+void rotate(int mod) {
+	if (mod == 0) {
+		if (now == 0)now = 1;
+		else if (now == 1)now =0;
+	}
+	else if (mod == 1) {
+		if (now == 1) {
+			now = 2;
+		}
+		else if (now == 2) {
+			now = 1;
+		}
+	}
+	else {
+		if (now == 2)now = 0;
+		else if (now == 0)now = 2;
+	}
+}
+
+int main() {
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++) {
+		char tmp = str[i];
+		int now = tmp - 'A';
+		rotate(now);
+	}
+	cout << now + 1 << '\n';
+}
