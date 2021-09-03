6aa78fe2f35f673932069fef54d23efeba75dc9a
rkdwlss2 <rkdwlss2@gmail.com>
2021-09-04 00:05:15 +0900

    boj5576

--- Project3/boj2480.cpp
+++ Project3/boj2480.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int arr[3];
-	int Big = -1;
-	for (int i = 0; i < 3; i++) {
-		cin >> arr[i];
-	}
-	for (int i = 0; i < 3; i++) {
-		if (arr[i] > Big)Big = arr[i];
-	}
-	if (arr[0] == arr[1] && arr[1] == arr[2] && arr[2] == arr[0]) {
-		cout << 10000 + arr[0] * 1000 << '\n';
-	}
-	else if (arr[0] != arr[1] && arr[1] != arr[2] && arr[2] != arr[0]) {
-		cout << Big * 100 << '\n';
-	}
-	else {
-		int Cnt = 0;
-		int Small = -1;
-		for (int i = 0; i < 3; i++) {
-			if (arr[i] == Big)Cnt++;
-			else Small = arr[i];
-		}
-		if (Cnt == 2) {
-			cout << 1000 + Big * 100 << '\n';
-		}
-		else {
-			cout << 1000 + Small * 100 << '\n';
-		}
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int arr[3];
+//	int Big = -1;
+//	for (int i = 0; i < 3; i++) {
+//		cin >> arr[i];
+//	}
+//	for (int i = 0; i < 3; i++) {
+//		if (arr[i] > Big)Big = arr[i];
+//	}
+//	if (arr[0] == arr[1] && arr[1] == arr[2] && arr[2] == arr[0]) {
+//		cout << 10000 + arr[0] * 1000 << '\n';
+//	}
+//	else if (arr[0] != arr[1] && arr[1] != arr[2] && arr[2] != arr[0]) {
+//		cout << Big * 100 << '\n';
+//	}
+//	else {
+//		int Cnt = 0;
+//		int Small = -1;
+//		for (int i = 0; i < 3; i++) {
+//			if (arr[i] == Big)Cnt++;
+//			else Small = arr[i];
+//		}
+//		if (Cnt == 2) {
+//			cout << 1000 + Big * 100 << '\n';
+//		}
+//		else {
+//			cout << 1000 + Small * 100 << '\n';
+//		}
+//	}
+//}
---
+++ Project3/boj5576.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int a[11];
+int w[11];
+int main() {
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		cin >> w[i];
+	}
+	sort(a, a + 10);
+	sort(w, w + 10);
+	cout << a[9] + a[8] + a[7] << '\n';
+	cout << w[9] + w[8] + w[7] << '\n';
+}
