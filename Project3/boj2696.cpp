6cf8b31c72cdd24d2ef2a9e7074fa101b56ae6ec
rkdwlss2 <rkdwlss2@gmail.com>
2021-09-06 22:39:55 +0900

    boj2357

--- Project3/boj2696.cpp
+++ Project3/boj2696.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <queue>
-#define lli long long int
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	lli n;
-	cin >> n;
-	priority_queue<lli> pq;
-	for (lli i = 0; i < n; i++) {
-		for (lli j = 0; j < n; j++) {
-			lli tmp;
-			cin >> tmp;
-			lli Small=-98765432100;
-			if (!pq.empty()) {
-				Small = -pq.top();
-			}
-			
-			if (Small >= tmp) {
-				continue;
-			}
-			pq.push(-tmp);
-			if (pq.size() > n) {
-				pq.pop();
-			}
-			
-		}
-	}
-	lli answer=-pq.top();
-	cout << answer << '\n';
-}
+//#include <iostream>
+//#include <queue>
+//#define lli long long int
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	lli n;
+//	cin >> n;
+//	priority_queue<lli> pq;
+//	for (lli i = 0; i < n; i++) {
+//		for (lli j = 0; j < n; j++) {
+//			lli tmp;
+//			cin >> tmp;
+//			lli Small=-98765432100;
+//			if (!pq.empty()) {
+//				Small = -pq.top();
+//			}
+//			
+//			if (Small >= tmp) {
+//				continue;
+//			}
+//			pq.push(-tmp);
+//			if (pq.size() > n) {
+//				pq.pop();
+//			}
+//			
+//		}
+//	}
+//	lli answer=-pq.top();
+//	cout << answer << '\n';
+//}
