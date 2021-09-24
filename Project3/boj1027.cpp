9f273602fd7d42685cf2fd05f88e531b6001cf14
rkdwlss2 <rkdwlss2@gmail.com>
2021-09-25 00:44:25 +0900

    boj1027

--- Project3/boj1027.cpp
+++ Project3/boj1027.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
+#define lli long long int
 using namespace std;
 
-int hight[51];
+lli hight[51];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n;
+	lli n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (lli i = 0; i < n; i++) {
 		cin >> hight[i];
 	}
-	int answer = 0;
-	for (int s = 0; s < n; s++) {
-		int cnt = 0;
-		for (int e = 0; e < n; e++) {
+	lli answer = 0;
+	for (lli s = 0; s < n; s++) {
+		lli cnt = 0;
+		for (lli e = 0; e < n; e++) {
 			if (s == e)continue;
-			int start = s < e ? s : e;
-			int end = s < e ? e : s;
-			int width = end - start;
-			int height =hight[e]-hight[s];
+			lli start = s < e ? s : e;
+			lli end = s < e ? e : s;
+			lli width = end - start;
+			lli height =hight[e]-hight[s];
 			bool ok = true;
-			for (int i = start+1; i < end; i++) {
-				int w = i > s ? i - s : s - i;
-				int h = hight[i] - hight[s];
+			for (lli i = start+1; i < end; i++) {
+				lli w = i > s ? i - s : s - i;
+				lli h = hight[i] - hight[s];
 				if (height * w <= h * width) {
 					ok = false;
 					break;
