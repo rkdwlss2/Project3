314f0536dc3d0178d356a9fee57f49cca674a7f3
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-02 22:25:22 +0900

    boj15681

--- Project3/boj1450.cpp
+++ Project3/boj1450.cpp
@@ -1,31 +1,31 @@
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int n, c, ans, a[33];
-vector<int> v1, v2;
-
-void dfs(int s, int e, int sum, vector<int>& v) {
-	if (sum > c) return;
-	if (s > e) return v.push_back(sum);
-	dfs(s + 1, e, sum, v);
-	dfs(s + 1, e, sum + a[s], v);
-}
-
-int main() {
-	scanf("%d %d", &n, &c);
-	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-
-	dfs(0, n / 2 - 1, 0, v1); sort(v1.begin(), v1.end());
-	dfs(n / 2, n - 1, 0, v2); sort(v2.begin(), v2.end());
-
-	int e = v2.size() - 1;
-	for (int s = 0; s < v1.size(); s++) {
-		while (e >= 0 && v1[s] + v2[e] > c) e--;
-		ans += e + 1;
-	}
-	printf("%d", ans);
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int n, c, ans, a[33];
+//vector<int> v1, v2;
+//
+//void dfs(int s, int e, int sum, vector<int>& v) {
+//	if (sum > c) return;
+//	if (s > e) return v.push_back(sum);
+//	dfs(s + 1, e, sum, v);
+//	dfs(s + 1, e, sum + a[s], v);
+//}
+//
+//int main() {
+//	scanf("%d %d", &n, &c);
+//	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+//
+//	dfs(0, n / 2 - 1, 0, v1); sort(v1.begin(), v1.end());
+//	dfs(n / 2, n - 1, 0, v2); sort(v2.begin(), v2.end());
+//
+//	int e = v2.size() - 1;
+//	for (int s = 0; s < v1.size(); s++) {
+//		while (e >= 0 && v1[s] + v2[e] > c) e--;
+//		ans += e + 1;
+//	}
+//	printf("%d", ans);
+//
+//	return 0;
+//}
---
+++ Project3/boj15681.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, r, q;
+int subtree[100001];
+vector<int> graph[100001];
+bool visited[100001];
+
+int go(int prev) {
+	int sum = 0;
+	bool check = false;
+	for (int i = 0 ; i <graph[prev].size();i++){
+		int next = graph[prev][i];
+		if (visited[next] == true)continue;
+		visited[next] = true;
+		sum += go(next);
+		check = true;
+	}
+	if (!check)return 1;
+	subtree[prev] = sum;
+	return 1 + sum;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> r >> q;
+	for (int i = 0; i < n-1; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	visited[r] = true;
+	go(r);
+	for (int i = 0; i < q; i++) {
+		int s;
+		cin >> s;
+		cout << subtree[s] + 1 << '\n';
+	}
+}
