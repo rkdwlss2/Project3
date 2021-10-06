5a265ea43ef1a784ec590ce3e5a645d70e816015
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-06 21:50:47 +0900

    boj5337

--- Project3/boj4562.cpp
+++ Project3/boj4562.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int a, b;
-		cin >> a >> b;
-		if (a >= b) {
-			cout << "MMM BRAINS" << '\n';
-		}
-		else {
-			cout << "NO BRAINS" << '\n';
-		}
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		int a, b;
+//		cin >> a >> b;
+//		if (a >= b) {
+//			cout << "MMM BRAINS" << '\n';
+//		}
+//		else {
+//			cout << "NO BRAINS" << '\n';
+//		}
+//	}
+//}
---
+++ Project3/boj4892.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int index = 0;
+//	while (true) {
+//		int a;
+//		cin >> a;
+//		if (a == 0)break;
+//		index++;
+//		if ((3*a) % 2 == 1) {
+//			cout << index << ". "<<"odd " << a << '\n';
+//		}
+//		else {
+//			cout << index << ". " << "even " << a << '\n';
+//		}
+//	}
+//}
