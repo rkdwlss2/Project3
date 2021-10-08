4ea6985067a3de96b0f81d9c1e166b9ea5314cab
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-08 20:01:42 +0900

    boj5338

--- Project3/boj5337.cpp
+++ Project3/boj5337.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << ".  .   ." << '\n';
-	cout << "|  | _ | _. _ ._ _  _" << '\n';
-	cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << ".  .   ." << '\n';
+//	cout << "|  | _ | _. _ ._ _  _" << '\n';
+//	cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+//}
