89f2d60e661680276e73b4f5b279ad9ccab36705
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-11 19:05:09 +0900

    boj5217

--- Project3/boj2693.cpp
+++ Project3/boj2693.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int main() {
-	int tc;
-	cin >> tc;
-	for (int i = 0; i < tc; i++) {
-		vector<int> v;
-		for (int j = 0; j < 10; j++) {
-			int tmp;
-			cin >> tmp;
-			v.push_back(tmp);
-		}
-		sort(v.begin(), v.end());
-		cout << v[7] << '\n';
-	}
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main() {
+//	int tc;
+//	cin >> tc;
+//	for (int i = 0; i < tc; i++) {
+//		vector<int> v;
+//		for (int j = 0; j < 10; j++) {
+//			int tmp;
+//			cin >> tmp;
+//			v.push_back(tmp);
+//		}
+//		sort(v.begin(), v.end());
+//		cout << v[7] << '\n';
+//	}
+//}
---
+++ Project3/boj5217.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		
+		int a;
+		cin >> a;
+		cout << "Pairs for " << a << ":" ;
+		for (int i = 0; i < a/ 2 ; i++) {
+			
+			int First = i + 1;
+			int Second = a - i - 1;
+			if (First == Second)break;
+			if (i != 0)cout << ",";
+			cout << " " << First << " " << Second;
+		}
+		cout << '\n';
+	}
+}
