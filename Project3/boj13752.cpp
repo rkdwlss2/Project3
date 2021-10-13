436adf6aac10a7717a8f05344a582dd18a09c181
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-13 21:46:15 +0900

    boj12995

--- Project3/boj13752.cpp
+++ Project3/boj13752.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
-		for (int j = 0; j < tmp; j++) {
-			cout << "=";
-		}
-		cout << '\n';
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		int tmp;
+//		cin >> tmp;
+//		for (int j = 0; j < tmp; j++) {
+//			cout << "=";
+//		}
+//		cout << '\n';
+//	}
+//}
