6819b520119c72280ffc7c24d011a89d8d964ec9
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-14 22:27:52 +0900

    boj15700

---
+++ Project3/boj15700.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define lli long long int
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	lli n, m;
+	cin >> n >> m;
+	cout << (n * m) / 2 << '\n';
+}
