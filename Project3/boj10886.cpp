f31c1b764d13d26266186ae25ce11bc6f05eb11e
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-20 22:08:14 +0900

    boj10886

---
+++ Project3/boj10886.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,cute=0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		if (tmp == 1)cute++;
+	}
+	int notCute = n - cute;
+	if (notCute > cute) {
+		cout << "Junhee is not cute!" << '\n';
+	}
+	else {
+		cout << "Junhee is cute!" << '\n';
+	}
+}
--- Project3/boj1351.cpp
+++ Project3/boj1351.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <map>
-using namespace std;
-
-long long n, p, q;
-
-map<long long, long long> m;
-
-long long go(long long n) {
-	if (n == 0)return 1;
-	if (m.count(n) > 0) {
-		return m[n];
-	}
-	long long now = go(n / p) + go(n / q);
-	m.insert({ n,now });
-	return now;
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> p >> q;
-	cout << go(n) << '\n';
-}
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//
+//long long n, p, q;
+//
+//map<long long, long long> m;
+//
+//long long go(long long n) {
+//	if (n == 0)return 1;
+//	if (m.count(n) > 0) {
+//		return m[n];
+//	}
+//	long long now = go(n / p) + go(n / q);
+//	m.insert({ n,now });
+//	return now;
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cin >> n >> p >> q;
+//	cout << go(n) << '\n';
+//}
