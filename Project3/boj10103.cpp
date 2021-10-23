ede87f56477064fd89541b01c58ef828b9a66e20
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-23 20:09:30 +0900

    boj1072

--- Project3/boj10103.cpp
+++ Project3/boj10103.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	int aScore = 100;
-	int bScore = 100;
-	for (int i = 0; i < n; i++) {
-		int a, b;
-		cin >> a >> b;
-		if (a > b) {
-			aScore -= a;
-		}
-		else if (a < b) {
-			bScore -= b;
-		}
-	}
-	cout << bScore << '\n';
-	cout << aScore << '\n';
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	int aScore = 100;
+//	int bScore = 100;
+//	for (int i = 0; i < n; i++) {
+//		int a, b;
+//		cin >> a >> b;
+//		if (a > b) {
+//			aScore -= a;
+//		}
+//		else if (a < b) {
+//			bScore -= b;
+//		}
+//	}
+//	cout << bScore << '\n';
+//	cout << aScore << '\n';
+//}
---
+++ Project3/boj1072.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1500000000;
+
+int main()
+{
+	int res = -1;
+	long long x, y;
+	int z;
+
+	while (cin >> x >> y)
+	{
+		long long left = 0, right = MAX;
+		z = 100 * y / x;
+
+		if (z >= 99)
+			left = -1;
+		else
+		{
+			while (left <= right)
+			{
+				long long middle = (left + right) / 2;
+				long long cal = 100 * (y + middle) / (x + middle);
+
+				if (cal <= z)
+					left = middle + 1;
+				else
+					right = middle - 1;
+			}
+
+		}
+		cout << left << '\n';
+	}
+
+	return 0;
+}
