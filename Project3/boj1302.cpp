9663271b5a6d2a81cd86992a3bc3da0c7f89b0bd
rkdwlss2 <rkdwlss2@gmail.com>
2021-10-28 22:49:13 +0900

    boj9316

--- Project3/boj1302.cpp
+++ Project3/boj1302.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	unordered_map<string, int> um;
-	for (int i = 0; i < n; i++) {
-		string tmp;
-		cin >> tmp;
-		if (um.count(tmp) > 0) {
-			um[tmp]++;
-		}
-		else {
-			um.insert({ tmp,1 });
-		}
-	}
-	vector<pair<int, string>> v;
-	for (auto i : um) {
-		v.push_back({ i.second,i.first });
-	}
-	sort(v.begin(), v.end());
-	int Big = v[v.size() - 1].first;
-	for (int i = 0; i < v.size(); i++) {
-		if (v[i].first == Big) {
-			cout << v[i].second << '\n';
-			return 0;
-		}
-	}
-}
+//#include <iostream>
+//#include <string>
+//#include <unordered_map>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int n;
+//	cin >> n;
+//	unordered_map<string, int> um;
+//	for (int i = 0; i < n; i++) {
+//		string tmp;
+//		cin >> tmp;
+//		if (um.count(tmp) > 0) {
+//			um[tmp]++;
+//		}
+//		else {
+//			um.insert({ tmp,1 });
+//		}
+//	}
+//	vector<pair<int, string>> v;
+//	for (auto i : um) {
+//		v.push_back({ i.second,i.first });
+//	}
+//	sort(v.begin(), v.end());
+//	int Big = v[v.size() - 1].first;
+//	for (int i = 0; i < v.size(); i++) {
+//		if (v[i].first == Big) {
+//			cout << v[i].second << '\n';
+//			return 0;
+//		}
+//	}
+//}
